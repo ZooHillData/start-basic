3f327639d13808c515bed9eeb30530e1fcb653cb
Alex <art@zoohilldata.com>
2025-02-19 18:54:13 -0700

    try NoSSR wrapping

--- app/routes/test-plotly.tsx
+++ app/routes/test-plotly.tsx
@@ -1,12 +1,23 @@
+import React from 'react'
+
 import { createFileRoute } from '@tanstack/react-router'
 import { Plotly } from '../components/plotly'
 export const Route = createFileRoute('/test-plotly')({
   component: TestPlotlyComponent,
-  ssr: false
+  // ssr: false
 })
+const NoSSR = ({ children }: Readonly<{ children: React.ReactNode }>) =>  {
+  const [isClient, setIsClient] = React.useState(false);
+  React.useEffect(() => {
+    setIsClient(true);
+  }, []);
+  return <>{isClient ? children : null}</>;
+}
 
 function TestPlotlyComponent() {
   return (
-    <Plotly />
+    <NoSSR>
+      <Plotly />
+    </NoSSR>
   )
 } 
