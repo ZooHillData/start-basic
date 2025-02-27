9bc6692e90d495206e0215e6a25ac70186a9cfe1
Alex <art@zoohilldata.com>
2025-02-27 13:51:52 -0700

    add node polyfills to fix plotly

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from '@tanstack/start/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
   vite: {
@@ -7,6 +8,7 @@ export default defineConfig({
       tsConfigPaths({
         projects: ['./tsconfig.json'],
       }),
+      nodePolyfills(),
     ],
   },
   tsr: {
--- app/routes/test-plotly.tsx
+++ app/routes/test-plotly.tsx
@@ -4,6 +4,7 @@ import { createFileRoute } from '@tanstack/react-router'
 import { Plotly } from '../components/plotly'
 export const Route = createFileRoute('/test-plotly')({
   component: TestPlotlyComponent,
+  ssr: true
 })
 const NoSSR = ({ children }: Readonly<{ children: React.ReactNode }>) =>  {
   const [isClient, setIsClient] = React.useState(false);
